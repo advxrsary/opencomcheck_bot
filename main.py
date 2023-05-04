b7956eae9cf16cbd070f85250e0faeed6ddb2e36
Sevastian Zare <sevastian.zare@knf.stud.vu.lt>
2023-05-04 17:43:12 +0300

    Add user tracking, TELEGRAM_USER_ID input prompt, and /list_users command
    - Add TELEGRAM_USER_ID prompt if unset in the .env file
    - Create and initialize SQLite database for user tracking
    - Implement UserTrackingMiddleware and add it to the dispatcher
    - Add /list_users command to display user data from the database in a readable format
    - Refactor check_channels to accept telethon_client as a parameter
    - Add error handling for NetworkError in the main polling loop

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
-import asyncio
 import re
 import logging
+import sqlite3
 import tempfile
+import time
+from datetime import datetime
+from dotenv import load_dotenv
 from typing import List
-from contextlib import asynccontextmanager
-from aiogram.utils.exceptions import BadRequest
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode, InputFile
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.types import InputFile
+from aiogram.utils.exceptions import NetworkError
 from aiogram.utils import executor
+from contextlib import asynccontextmanager
 from telethon import TelegramClient
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.errors.rpcerrorlist import UsernameNotOccupiedError
-from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -41,6 +45,11 @@ To use the bot:
 api_id = os.environ.get("TELEGRAM_API_ID")
 api_hash = os.environ.get("TELEGRAM_API_HASH")
 bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_USER_ID = os.environ.get("TELEGRAM_USER_ID")
+
+if not TELEGRAM_USER_ID:
+    TELEGRAM_USER_ID = input("TELEGRAM_USER_ID is unset in '.env'. Please enter TELEGRAM_USER_ID: ")
+
 
 @asynccontextmanager
 async def get_telethon_client():
@@ -50,7 +59,6 @@ async def get_telethon_client():
         yield telethon_client
     finally:
         await telethon_client.disconnect()
-        await bot.close()
 
 bot = Bot(token=bot_token)
 dp = Dispatcher(bot)
@@ -59,24 +67,93 @@ dp.middleware.setup(LoggingMiddleware())
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+def init_db():
+    conn = sqlite3.connect("bot_users.db")
+    c = conn.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT UNIQUE,
+            timestamp DATETIME
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+init_db()
+
+def add_user(username: str):
+    conn = sqlite3.connect("bot_users.db")
+    c = conn.cursor()
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        c.execute("""
+            INSERT OR IGNORE INTO users (username, timestamp) 
+            VALUES (?, ?)
+        """, (username, timestamp))
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error adding user {username}: {e}")
+    finally:
+        conn.close()
+
+async def track_user_middleware(event: types.Update, next_call):
+    if event.message and event.message.from_user.username:
+        add_user(event.message.from_user.username)
+    await next_call()
+
+class UserTrackingMiddleware(BaseMiddleware):
+
+    async def on_pre_process_message(self, message: types.Message, data: dict):
+        if message.from_user.username:
+            add_user(message.from_user.username)
+
+dp.middleware.setup(UserTrackingMiddleware())
+
+
+def get_users():
+    conn = sqlite3.connect("bot_users.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM users")
+    users = c.fetchall()
+    conn.close()
+
+    return "\n".join(f"{user[1]} (ID: {user[0]}, Timestamp: {user[2]})" for user in users)
+
+
+@dp.message_handler(commands=['list_users'])
+async def list_users(message: types.Message):
+    if str(message.from_user.id) == TELEGRAM_USER_ID:
+        users_data = get_users()
+        await message.reply(f"Users:\n{users_data}")
+    else:
+        await message.reply("You are not authorized to use this command.")
+
+
 async def on_startup(dp):
     pass
 
+
 async def on_shutdown(dp):
     pass
+
+
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def start_help(message: types.Message):
     await message.reply(BANNER)
 
-async def check_channels(channels: List[str]) -> List[str]:
+
+async def check_channels(telethon_client, channels: List[str]) -> List[str]:
     open_comments = []
     not_existing_channels = []
 
     for channel_username in channels:
         try:
             channel = await telethon_client.get_entity(channel_username)
-            if not channel.admin_rights or channel.admin_rights.post_messages:
+            full_channel = await telethon_client(GetFullChannelRequest(channel))
+            if full_channel.full_chat.linked_chat_id:
                 open_comments.append(channel_username)
         except ValueError as e:
             not_existing_channels.append(channel_username)
@@ -87,6 +164,7 @@ async def check_channels(channels: List[str]) -> List[str]:
 
     return open_comments
 
+
 async def send_open_comments_channels(chat_id: int, open_comments: List[str]):
     if not open_comments:
         await bot.send_message(chat_id=chat_id, text="No channels with open comments found.")
@@ -94,7 +172,7 @@ async def send_open_comments_channels(chat_id: int, open_comments: List[str]):
 
     with tempfile.NamedTemporaryFile(mode="w+", suffix=".txt", delete=False) as f:
         f.write("\n".join(open_comments))
-        f.flush()  
+        f.flush()
     with open(f.name, "rb") as file_to_send:
         await bot.send_document(chat_id=chat_id, document=InputFile(file_to_send), caption="Channels with open comments section")
 
@@ -106,7 +184,8 @@ async def list_channels(message: types.Message):
     channels = re.findall(r"@[\w\d]+", message.text)
     await message.reply(f"Checking {len(channels)} channels. Please wait...")
 
-    open_comments = await check_channels(channels)
+    async with get_telethon_client() as telethon_client:
+        open_comments = await check_channels(telethon_client, channels)
 
     await send_open_comments_channels(message.chat.id, open_comments)
 
@@ -116,15 +195,26 @@ async def handle_file(message: types.Message):
     document = message.document
     file_bytes = await bot.download_file_by_id(document.file_id)
 
-    channels = [line.strip() for line in file_bytes.getvalue().decode().split('\n')]
+    channels = [line.strip()
+                for line in file_bytes.getvalue().decode().split('\n')]
 
     await message.reply(f"Checking {len(channels)} channels. Please wait...")
 
-    open_comments = await check_channels(channels)
+    async with get_telethon_client() as telethon_client:
+        open_comments = await check_channels(telethon_client, channels)
 
     await send_open_comments_channels(message.chat.id, open_comments)
 
 
+
 if __name__ == '__main__':
-    from aiogram import executor
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    while True:
+        try:
+            executor.start_polling(
+                dp, on_startup=on_startup, on_shutdown=on_shutdown)
+        except NetworkError as e:
+            logging.error(
+                f"NetworkError occurred: {e}. Retrying in 5 seconds...")
+            time.sleep(5)
+        else:
+            break
